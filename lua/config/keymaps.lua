f1752c29ae809de84be077e6ad19f62cc1bd8236
huangjianchao <hjc0720@126.com>
2024-10-28 11:04:53 +0800

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.del({"v","n"},"s")
+-- vim.keymap.del("n","<leader>S")
+
+vim.keymap.set("n","<leader>m","<cmd><leader>ss<CR>",{desc = "find doc symbols"})
+local builtin = require('telescope.builtin')
+vim.keymap.set("n","<leader>O",builtin.git_files,{desc = "find file in git"})
+vim.keymap.set("n","<leader>S",builtin.lsp_dynamic_workspace_symbols,{desc = "find symbols in git"})
+
+local function cmakeBuildAll()
+	require('cmake-tools').build({target="all"})
+end
+
+vim.keymap.set("n","<leader>mb",cmakeBuildAll,{desc = "cmake build file"})
+
---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+opt.autochdir = true
+
+opt.expandtab = false
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.fileencodings={"ucs-bom","utf-8","cp936","gbk","latin1","utf-16"}
+opt.relativenumber = false
+opt.termguicolors = true
