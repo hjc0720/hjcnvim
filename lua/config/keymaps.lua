a7a69be8b06821a3ae3144f6c2817334906cf780
huangjianchao <hjc@xszn-tech.com>
2024-11-12 14:00:22 +0800

    优化keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,17 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del({"v","n"},"s")
+vim.keymap.del({ "v", "n" }, "s")
 -- vim.keymap.del("n","<leader>S")
 
-vim.keymap.set("n","<leader>m","<cmd><leader>ss<CR>",{desc = "find doc symbols"})
-local builtin = require('telescope.builtin')
-vim.keymap.set("n","<leader>O",builtin.git_files,{desc = "find file in git"})
-vim.keymap.set("n","<leader>S",builtin.lsp_dynamic_workspace_symbols,{desc = "find symbols in git"})
+vim.keymap.set("n", "<leader>m", "<cmd><leader>ss<CR>", { desc = "find doc symbols" })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>O", builtin.git_files, { desc = "find file in git" })
+vim.keymap.set("n", "<leader>S", builtin.lsp_dynamic_workspace_symbols, { desc = "find symbols in git" })
 
 local function cmakeBuildAll()
-	require('cmake-tools').build({target="all"})
+  require("cmake-tools").build({ target = "all" })
 end
 
-vim.keymap.set("n","<leader>mb",cmakeBuildAll,{desc = "cmake build file"})
-
+-- vim.keymap.set("n","<leader>mb",cmakeBuildAll,{desc = "cmake build file"})
+vim.keymap.set("n", "<leader>mb", "<CMD>CMakeBuildCurrentFile<CR>", { desc = "cmake build file" })
